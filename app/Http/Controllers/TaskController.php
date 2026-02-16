47a36e40d108bc5348c692361b4ec7d4c868b259
1t-pensan <ppatrik0319@gmail.com>
2026-02-16 08:35:19 +0100

    .

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = task::all();
+        return response()->json($tasks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $task = task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(task $task)
+    {
+        return response()->json($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, task $task)
+    {
+        $task->update($request->all());
+        return response()->json($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(task $task)
+    {
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
